83838baf56e711d9e68b62e61123134bd5ad7eb8
staniboy <stanley.v@outlook.com>
2022-12-09 12:27:24 +0400

    feat: alert msg

--- src/components/AppLoginForm.vue
+++ src/components/AppLoginForm.vue
@@ -1,4 +1,13 @@
 <template>
+  <!-- Alert Message -->
+  <div
+    v-if="alert.displayAlert"
+    class="text-white text-center font-bold p-4 mb-4 rounded"
+    :class="alert.cssClass"
+  >
+    {{ alert.alertMessage }}
+  </div>
+  <!--  Vee Form -->
   <vee-form :validation-schema="loginSchema" @submit="login">
     <!-- Email -->
     <div class="mb-3">
@@ -24,6 +33,7 @@
     </div>
     <button
       type="submit"
+      :disabled="alert.inProgress"
       class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
     >
       Submit
@@ -40,11 +50,26 @@ export default {
         email: "required|email",
         password: "required|min:9|max:100",
       },
+      alert: {
+        inProgress: false,
+        displayAlert: false,
+        cssClass: "bg-blue-500",
+        alertMessage: "Please wait! We are logging you in.",
+      },
     };
   },
   methods: {
     login(values) {
-      console.log(values);
+      this.alert.displayAlert = true;
+      this.alert.inProgress = true;
+      this.alert.cssClass = "bg-blue-500";
+      this.alert.alertMessage = "Please wait! We are logging you in.";
+      setTimeout(() => {
+        this.alert.inProgress = false;
+        this.alert.cssClass = "bg-green-500";
+        this.alert.alertMessage = "Success! Your are logged in.";
+        console.log(values);
+      }, 3000);
     },
   },
 };
