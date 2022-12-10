05d65ba94bed0ff04117d0c3699326c2483834a3
staniboy <stanley.v@outlook.com>
2022-12-10 21:40:48 +0400

    feat: check if user is logged in

--- src/App.vue
+++ src/App.vue
@@ -273,10 +273,22 @@
 <script>
 import AppHeader from "@/components/AppHeader.vue";
 import AppAuth from "@/components/AppAuth.vue";
+import { auth } from "@/includes/firebase";
+import { mapWritableState } from "pinia";
+import useUserStore from "@/stores/user";
+
 export default {
   components: {
     AppHeader,
     AppAuth,
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ["isLoggedIn"]),
+  },
+  created() {
+    if (auth.currentUser) {
+      this.isLoggedIn = true;
+    }
   },
 };
 </script>
