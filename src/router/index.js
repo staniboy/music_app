f31bd2e2af19cfb415c5855e9859148c3f8ff138
staniboy <stanley.v@outlook.com>
2022-12-12 15:18:45 +0400

    feat: catch all redirect

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,10 @@ const routes = [
     path: "/manage",
     component: ManageView,
   },
+  {
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" },
+  },
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
