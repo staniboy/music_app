643c24d8b3180b419907c435c9886323c500f925
staniboy <stanley.v@outlook.com>
2022-12-12 23:24:57 +0400

    feat: better route guards

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,22 +7,25 @@
         class="text-white font-bold uppercase text-2xl mr-4"
         :to="{ name: 'home' }"
         exact-active-class="no-active"
-        >Music</router-link
-      >
-
+        >Music
+      </router-link>
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li>
-            <router-link class="px-2 text-white" :to="{ name: 'about' }"
-              >About</router-link
-            >
+            <router-link class="px-2 text-white" :to="{ name: 'about' }">
+              About
+            </router-link>
           </li>
           <li v-if="!userStore.isLoggedIn">
-            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
-              >Login / Register</a
+            <a
+              class="px-2 text-white"
+              href="#"
+              @click.prevent="toggleAuthModal"
             >
+              Login / Register
+            </a>
           </li>
           <template v-else>
             <li>
@@ -31,10 +34,7 @@
               </router-link>
             </li>
             <li>
-              <a
-                class="px-2 text-white"
-                href="#"
-                @click.prevent="userStore.signOut"
+              <a class="px-2 text-white" href="#" @click.prevent="signOut"
                 >Logout</a
               >
             </li>
@@ -57,6 +57,12 @@ export default {
   methods: {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen;
+    },
+    signOut() {
+      this.userStore.signOut();
+      if (this.$route.meta.requiresAuth) {
+        this.$router.push({ name: "home" });
+      }
     },
   },
 };
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
 import ManageView from "@/views/ManageView.vue";
+import useUserStore from "@/stores/user";
 
 const routes = [
   {
@@ -19,6 +20,9 @@ const routes = [
     // alias: "manage",
     path: "/manage-music",
     component: ManageView,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: "/manage",
@@ -33,6 +37,18 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   linkExactActiveClass: "text-amber-500",
+});
+
+router.beforeEach((to, from, next) => {
+  if (!to.meta.requiresAuth) {
+    next();
+    return;
+  }
+  if (useUserStore().isLoggedIn) {
+    next();
+  } else {
+    next({ name: "home" });
+  }
 });
 
 export default router;
